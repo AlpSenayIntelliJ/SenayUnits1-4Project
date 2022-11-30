137b0c9be8fdfa05dbaf08d02ee6b90b35bf2135
Alp Senay <alps2789@nycstudents.net>
2022-11-30 12:29:18 -0500

    working...

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to Road Trip!");
-        //trip.border();
-        System.out.print("What is your name?");
-        String name = scan.nextLine();
         System.out.println();
-        System.out.print("Hey " + name + ", how old are you?");
+        System.out.println("Welcome to Road Trip!");
+        System.out.println("---------------------");
+        System.out.print("What is your name? ");
+        String name = scan.nextLine();
+        System.out.print("Hey " + name + ", how old are you? ");
         int age = scan.nextInt();
-        //trip.checkIfCanDrive();
+        RoadTrip trip = new RoadTrip(name, age);
+        trip.checkIfCanDrive();
+        trip.pickCar();
+
+
     }
 
 }
--- src/RoadTrip.java
+++ src/RoadTrip.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class RoadTrip {
+    Scanner scan = new Scanner(System.in);
 
     private String name;
     private int age;
@@ -7,22 +9,43 @@ public class RoadTrip {
 
     public RoadTrip() { money = 1000; };
 
-    public RoadTrip(String name, int age, String car) {
+    public RoadTrip(String name, int age) {
         this.name = name;
         this.age = age;
-        this.car = car;
-    }
-
-    public void border() {
-        System.out.println("Welcome to the Oregon Trail!");
     }
 
     public void checkIfCanDrive() {
         if (age >= 18) {
             System.out.println("Ok, you are eligible to drive!");
+            System.out.println("------------------------------");
         } else {
             System.out.println("Hey, you are under the age to drive!");
             System.exit(0);
+        }
+    }
+
+    public void pickCar() {
+        System.out.println("Select a vehicle for the road trip:");
+        System.out.println("A. Sedan car");
+        System.out.println("B. RV");
+        System.out.println("C. Pickup truck");
+        System.out.println("D. Golf cart");
+        String carPick = scan.nextLine();
+        if (carPick.equals("A")) {
+            System.out.println("Ok, you have chosen a sedan car! This car will go " + (int) (Math.random()*21+50) + " miles per hour");
+        }
+        else if (carPick.equals("B")) {
+            System.out.println("Ok, you have chosen an RV! This RV will go " + (int) (Math.random()*21+40) + " miles per hour");
+        }
+        else if (carPick.equals("C")) {
+            System.out.println("Ok, you have chosen a pickup truck! This truck will go " + (int) (Math.random()*21+50) + " miles per hour");
+        }
+        else if (carPick.equals("D")) {
+            System.out.println("Ok, you have chosen a golf cart! This cart will go " + (int) (Math.random()*6+10) + " miles per hour");
+        }
+        else {
+            System.out.println("Please enter a valid input");
+
         }
     }
 
