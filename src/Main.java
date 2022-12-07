a80eb271a03b463d1be94035499b32a60b358e60
Alp Senay <alps2789@nycstudents.net>
2022-12-07 12:32:17 -0500

    working...

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         trip.checkIfCanDrive();
         trip.pickCar();
         trip.pickDestination();
+        trip.play();
 
 
     }
--- src/RoadTrip.java
+++ src/RoadTrip.java
@@ -6,6 +6,7 @@ public class RoadTrip {
     private int age;
     private Vehicle vehicle;
     private int money;
+    private int hours;
 
     public RoadTrip() { money = 1000; };
 
@@ -51,8 +52,8 @@ public class RoadTrip {
             System.out.println("Select a destination:");
             System.out.println("a. Miami");
             System.out.println("b. Chicago");
-            System.out.println("c. Boston");
-            System.out.println("d. Washington DC");
+            System.out.println("c. Atlanta");
+            System.out.println("d. Montreal");
             destPick = scan.nextLine();
         }
         switch (destPick) {
@@ -65,16 +66,38 @@ public class RoadTrip {
                 destPick = "Chicago";
             }
             case "c" -> {
-                distance = 216;
-                destPick = "Boston";
+                distance = 871;
+                destPick = "Atlanta";
             }
             default -> {
-                distance = 226;
-                destPick = "Washington DC";
+                distance = 372;
+                destPick = "Montreal";
             }
         }
+        hours = distance / vehicle.getSpeed();
         System.out.println("Ok, you have chosen to go to " + destPick + "! Your " + vehicle +
-            " will get there in about " + (distance / vehicle.getSpeed()) + " hours!");
+                " will get there in about " + (hours) + " hours!");
+        System.out.println("------------------------------");
+    }
+
+     public void play() {
+        System.out.println("Let the road trip begin!");
+        System.out.println("------------------------------");
+        for (int i = 0; i < hours;) {
+            int hoursTrav = Math.min((int) (Math.random()*6+1), hours-i);
+            i += hoursTrav;
+            if (hoursTrav == 1) {
+                System.out.println("You have gone " + hoursTrav + " hour!");
+            } else {
+                System.out.println("You have gone " + hoursTrav + " hours!");
+            }
+            int crashChance = (int) (Math.random()*20+1);
+            if (crashChance == vehicle.getRisk()) {
+                System.out.print("Oh no! You have crashed!");
                 System.out.println("------------------------------");
+                System.exit(0);
+            }
+        }
+
     }
 }
